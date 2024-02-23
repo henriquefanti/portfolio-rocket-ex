3b88f452e0734e7357800a4fb34353f0b35113cb
henriquefanti <carloshenriquefanti@gmail.com>
2024-02-23 03:32:04 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//função para deslizar o botão
+function toggleMode() {
+  html.classList.toggle("light")
+  //Pegar a tag img
+  //query pesquisar por seletor
+  const img = document.querySelector("#profile img")
+
+  //Substituir a imagem
+  if (html.classList.contains("light")) {
+    // se estiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //SENÃO manter imagem normal
+    img.setAttribute("src","./assets/avatar.png")
+  }
+}
